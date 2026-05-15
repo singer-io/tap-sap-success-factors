28d926579802fed7e891d5dac930977c2c63d575
satyendra101 <satyendra.singh@qlik.com>
2026-05-15 14:18:34 +0530

    Bump dependencies version

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_sap_success_factors"],
     install_requires=[
-        "singer-python==6.3.0",
-        "requests==2.32.5",
+        "singer-python==6.8.0",
+        "requests==2.33.1",
         "backoff==2.2.1",
         "python-dateutil==2.9.0.post0",
     ],
