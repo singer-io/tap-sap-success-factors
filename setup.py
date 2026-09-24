b32fc8a741d4da598977217bc93b3a5aea2a2837
akkumar-qlik <akarsh.kumar@qlik.com>
2026-09-24 11:26:29 +0530

    Added changelog and bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="tap-sap-success-factors",
-    version="0.0.1",
+    version="0.0.2",
     description="Singer.io tap for extracting data from SAP SuccessFactors OData v2 APIs",
     author="Singer Community",
     url="http://singer.io",
